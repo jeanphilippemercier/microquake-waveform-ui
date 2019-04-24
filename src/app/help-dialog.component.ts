7b596c0d02b2ebf5a2ba979d3b1687e8da9135e6
mrizescu <mihaelarizescu@isti.com>
2019-04-24 12:28:51 -0500

    fix dialog lint error

--- src/app/help-dialog.component.ts
+++ src/app/help-dialog.component.ts
@@ -2,35 +2,6 @@ import { Component, Inject } from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 
 @Component({
-  selector: 'app-help-dialog',
-  templateUrl: './help-dialog.component.html',
-  styleUrls: ['./help-dialog.component.css']
-})
-export class HelpDialogComponent {
-
-
-    constructor(public dialog: MatDialog) {}
-
-    openDialog(): void {
-        if (this.dialog.openDialogs.length > 0) {
-          this.dialog.closeAll();
-        } else {
-          const help = HelpDialogSheetComponent;
-          const dialogRef = this.dialog.open(help, {
-            width: '600px',
-            hasBackdrop: false,
-            position: {
-              'bottom': '0',
-              'left': '0'
-            }
-          });
-
-        }
-    }
-
-}
-
-@Component({
   selector: 'app-help-dialog-dialog',
   templateUrl: './help-dialog-sheet.component.html',
 })
@@ -46,3 +17,30 @@ export class HelpDialogSheetComponent {
 
 }
 
+@Component({
+  selector: 'app-help-dialog',
+  templateUrl: './help-dialog.component.html',
+  styleUrls: ['./help-dialog.component.css']
+})
+export class HelpDialogComponent {
+
+
+    constructor(public dialog: MatDialog) {}
+
+    openDialog(): void {
+        if (this.dialog.openDialogs.length > 0) {
+          this.dialog.closeAll();
+        } else {
+          const dialogRef = this.dialog.open(HelpDialogSheetComponent, {
+            width: '600px',
+            hasBackdrop: false,
+            position: {
+              'bottom': '0',
+              'left': '0'
+            }
+          });
+
+        }
+    }
+
+}
