dfcac87239913b1b714192dcce3ad7b18306fa7b
Reza Jelveh <toyb0mb@gmx.net>
2019-04-25 18:38:24 +0800

    auth: change the authentication url

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { environment } from '../environments/environment';
 
 @Injectable()
 export class AuthService {
   constructor(private http: HttpClient) { }
 
   login(username: string, password: string): Observable<boolean> {
-    return this.http.post<{token: string}>('/api/v1/auth', {username: username, password: password})
+    return this.http.post<{token: string}>(environment.apiUrl + '/token-auth', {username: username, password: password})
       .pipe(
         map(result => {
           localStorage.setItem('access_token', result.token);
