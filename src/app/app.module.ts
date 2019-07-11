5afffb749f357a2cbce52462c7cb43ea8f919d83
Tomas Skoda <tomas.skoda@toptal.com>
2019-07-11 12:31:45 +0200

    refactor(all): move injectors to core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-// import { MatIconRegistry, MatIconModule } from '@angular/material';
-// import { DomSanitizer } from '@angular/platform-browser';
-import { ComponentsModule } from './components/components.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatDialogModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { HttpClientModule } from '@angular/common/http';
+// import { MatIconRegistry, MatIconModule } from '@angular/material';
+// import { DomSanitizer } from '@angular/platform-browser';
+
+import { ComponentsModule } from './components/components.module';
 import { DemoMaterialModule } from './material-modules';
 import { AppComponent } from './app.component';
 import { CatalogTreeModule } from './catalog-tree/catalog-tree.module';
-import { JWT_OPTIONS, JwtInterceptor, JwtModule } from '@auth0/angular-jwt';
-import { AuthService } from './core/services/auth.service';
-import { environment } from '../environments/environment';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { RefreshTokenInterceptor } from './core/interceptors/refresh-token-interceptor';
 import { HelpDialogComponent, HelpDialogSheetComponent } from './help-dialog/help-dialog.component';
 import { NgxLoadingModule } from 'ngx-loading';
 import { NgxPaginationModule } from 'ngx-pagination';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './app-routing.module';
 import { WaveformComponent } from './waveform/waveform.component';
@@ -25,15 +22,6 @@ import { SiteNetworkComponent } from './site-network/site-network.component';
 import { CoreModule } from './core/core.module';
 // import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 
-export function jwtOptionsFactory(authService: AuthService) {
-  return {
-    tokenGetter: () => {
-      return authService.getAccessToken();
-    },
-    whitelistedDomains: ['api.microquake.org', 'localhost'],
-    blacklistedRoutes: [`${environment.apiUrl}/api/token`]
-  };
-}
 
 @NgModule({
   declarations: [
@@ -53,13 +41,6 @@ export function jwtOptionsFactory(authService: AuthService) {
     CatalogTreeModule,
     FormsModule,
     HttpClientModule,
-    JwtModule.forRoot({
-      jwtOptionsProvider: {
-        provide: JWT_OPTIONS,
-        useFactory: jwtOptionsFactory,
-        deps: [AuthService]
-      }
-    }),
     DemoMaterialModule,
     MatDialogModule,
     // MatIconModule,
@@ -70,19 +51,6 @@ export function jwtOptionsFactory(authService: AuthService) {
     AppRoutingModule
   ],
   entryComponents: [HelpDialogComponent, HelpDialogSheetComponent],
-  providers: [
-    JwtInterceptor, // Providing JwtInterceptor allow to inject JwtInterceptor manually into RefreshTokenInterceptor
-    {
-      provide: HTTP_INTERCEPTORS,
-      useExisting: JwtInterceptor,
-      multi: true
-    },
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: RefreshTokenInterceptor,
-      multi: true
-    }
-  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,47 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { JWT_OPTIONS, JwtInterceptor, JwtModule } from '@auth0/angular-jwt';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { environment } from '../../environments/environment';
+import { AuthService } from './services/auth.service';
+import { RefreshTokenInterceptor } from './interceptors/refresh-token-interceptor';
+
+export function jwtOptionsFactory(authService: AuthService) {
+  return {
+    tokenGetter: () => {
+      return authService.getAccessToken();
+    },
+    whitelistedDomains: ['api.microquake.org', 'localhost'],
+    blacklistedRoutes: [`${environment.apiUrl}/api/token`]
+  };
+}
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    JwtModule.forRoot({
+      jwtOptionsProvider: {
+        provide: JWT_OPTIONS,
+        useFactory: jwtOptionsFactory,
+        deps: [AuthService]
+      }
+    }),
+  ],
+  providers: [
+    JwtInterceptor, // Providing JwtInterceptor allow to inject JwtInterceptor manually into RefreshTokenInterceptor
+    {
+      provide: HTTP_INTERCEPTORS,
+      useExisting: JwtInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: RefreshTokenInterceptor,
+      multi: true
+    }
+  ],
 })
 export class CoreModule { }
+
