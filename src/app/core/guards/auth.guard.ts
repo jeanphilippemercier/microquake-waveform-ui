3c126640b5f008c3caf790a985aa77c9e19d6f75
Tomas Skoda <tomas.skoda@toptal.com>
2019-07-12 21:00:48 +0200

    fix(core): auth guard redirect

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
+import { AuthService } from '@services/auth.service';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(private router: Router) { }
+  constructor(
+    private _router: Router,
+    private _authService: AuthService
+  ) { }
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    if (localStorage.getItem('access_token')) {
+
+    if (this._authService.isAuthenticated()) {
       return true;
     }
 
-    this.router.navigate(['login']);
+    this._router.navigate(['..']);
     return false;
   }
 }
