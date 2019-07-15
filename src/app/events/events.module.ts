b95ad9e18a033c193ee2f2c813f216de30dff7e1
Tomas Skoda <tomas.skoda@toptal.com>
2019-07-15 16:51:18 +0200

    feat(events): add event list page

--- src/app/events/events.module.ts
+++ src/app/events/events.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 
 import { SharedModule } from '@app/shared/shared.module';
+import { EventListComponent } from './pages/event-list/event-list.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    EventListComponent
+  ],
   imports: [
     SharedModule,
   ]
