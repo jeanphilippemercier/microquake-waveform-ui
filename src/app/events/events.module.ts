e5ad9b2dd1512abbfc66ea41421de96ece96a418
Tomas Skoda <tomas.skoda@toptal.com>
2019-07-17 14:26:24 +0200

    feat(events): add event detail page

--- src/app/events/events.module.ts
+++ src/app/events/events.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 
 import { SharedModule } from '@app/shared/shared.module';
+import { EventDetailComponent } from './pages/event-detail/event-detail.component';
 import { EventListComponent } from './pages/event-list/event-list.component';
-import { SiteNetworkComponent } from './components/site-network/site-network.component';
 import { NotifierComponent } from './components/notifier/notifier.component';
 import { EventsTreeComponent } from './components/catalog-tree/events-tree.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
+    EventDetailComponent,
     EventListComponent,
-    SiteNetworkComponent,
     NotifierComponent,
     SidebarComponent,
     EventsTreeComponent
