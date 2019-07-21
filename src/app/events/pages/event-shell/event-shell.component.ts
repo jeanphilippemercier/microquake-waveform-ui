9ef42cfe9b9b21a4b9171d138a12b4cea5ab7195
Tomas Skoda <tomas.skoda@toptal.com>
2019-07-21 22:16:28 +0200

    feat(events): add sidebar for event detail v2
    - add expandable panels for event list and event detail
    - edit waveform toolbar and event loading
    - add dynamic app toolbar
    - WIP: refactor event api service

--- src/app/events/pages/event-shell/event-shell.component.ts
+++ src/app/events/pages/event-shell/event-shell.component.ts
@@ -1,27 +1,32 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 
 import { AuthService } from '@services/auth.service';
 import { User } from '@interfaces/user.interface';
 import { MatDrawer } from '@angular/material';
+import { MenuService } from '@services/menu.service';
 
 @Component({
   selector: 'app-event-shell',
   templateUrl: './event-shell.component.html',
   styleUrls: ['./event-shell.component.scss']
 })
-export class EventShellComponent implements OnInit {
+export class EventShellComponent implements OnInit, AfterViewInit {
   user: User;
   @ViewChild('drawer') drawer: MatDrawer;
 
   constructor(
-    private _authService: AuthService
+    private _authService: AuthService,
+    private _menuService: MenuService
   ) { }
 
   async ngOnInit() {
     this._authService.loggedUser.subscribe(user => {
       this.user = user;
     });
+  }
 
+  ngAfterViewInit() {
+    this._menuService.init(this.drawer);
   }
 
   logoutClick() {
