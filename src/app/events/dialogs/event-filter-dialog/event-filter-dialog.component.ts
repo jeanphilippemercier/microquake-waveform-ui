3d2198d8959fa882a290936cc2da3d8d6cfecf6d
Tomas Skoda <tomas.skoda@toptal.com>
2019-07-28 11:35:09 +0200

    feat(events): add event-filter-dialog

---
+++ src/app/events/dialogs/event-filter-dialog/event-filter-dialog.component.ts
@@ -0,0 +1,86 @@
+import { Component, Inject, EventEmitter } from '@angular/core';
+import { MAT_DIALOG_DATA, MatRadioChange } from '@angular/material';
+import * as moment from 'moment';
+
+import { EventQuery, EventType, EvaluationStatus, EventEvaluationMode } from '@interfaces/event.interface';
+import { Site } from '@interfaces/site.interface';
+import { EventFilterDialogData } from '@interfaces/dialogs.interface';
+
+@Component({
+  selector: 'app-event-filter-dialog',
+  templateUrl: './event-filter-dialog.component.html',
+  styleUrls: ['./event-filter-dialog.component.scss']
+})
+export class EventFilterDialogComponent {
+  loading = false;
+  somethingEdited = false;
+  onFilter: EventEmitter<EventQuery> = new EventEmitter();
+  origQuery: EventQuery;
+  eventQuery: EventQuery;
+  editedQuery: EventQuery;
+  eventTypes: EventType[];
+  evaluationStatuses: EvaluationStatus[];
+  eventEvaluationModes: EventEvaluationMode[];
+  selectedSite: Site;
+  timezone: string;
+  todayEnd: Date;
+  numberOfChanges = 0;
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) private _dialogData: EventFilterDialogData
+  ) {
+    this.timezone = this._dialogData.timezone;
+    this.eventQuery = { ...this._dialogData.eventQuery };
+    this.editedQuery = { ...this._dialogData.eventQuery };
+    this.eventTypes = this._dialogData.eventTypes;
+    this.evaluationStatuses = this._dialogData.evaluationStatuses;
+    this.eventEvaluationModes = this._dialogData.eventEvaluationModes;
+    this.todayEnd = moment().utc().utcOffset(this.timezone).endOf('day').toDate();
+
+  }
+
+  async onFilterChange() {
+    this.somethingEdited = this.checkIfSomethingEdited(this.eventQuery, this.editedQuery);
+    this.numberOfChanges = this.getNumberOfChanges(this.editedQuery);
+  }
+
+  onFilterClick(event: EventQuery) {
+    this.onFilter.emit(event);
+  }
+
+  resetFilter() {
+    this.editedQuery = { ...this.origQuery };
+    this.editedQuery.time_range = 3;
+    this.editedQuery.start_time = moment()
+      .utcOffset(this.timezone)
+      .startOf('day')
+      .subtract(this.editedQuery.time_range, 'days')
+      .toISOString();
+    this.editedQuery.end_time = this.todayEnd.toISOString();
+    this.somethingEdited = this.checkIfSomethingEdited(this.eventQuery, this.editedQuery);
+    this.numberOfChanges = this.getNumberOfChanges(this.editedQuery);
+  }
+
+  onTimeRangeChange($event: MatRadioChange) {
+    if ($event.value !== 0) {
+      this.editedQuery.start_time = moment().utcOffset(this.timezone).startOf('day').subtract($event.value - 1, 'days').toISOString();
+      this.editedQuery.end_time = this.todayEnd.toISOString();
+    }
+    this.somethingEdited = this.checkIfSomethingEdited(this.eventQuery, this.editedQuery);
+    this.numberOfChanges = this.getNumberOfChanges(this.editedQuery);
+  }
+
+  checkIfSomethingEdited(origQuery: EventQuery, editedQuery: EventQuery): boolean {
+    return JSON.stringify(origQuery) !== JSON.stringify(editedQuery) ? true : false;
+  }
+
+  getNumberOfChanges(editedQuery: EventQuery): number {
+    let count = 0;
+    count += editedQuery.status ? editedQuery.status.length : 0;
+    count += editedQuery.type ? editedQuery.type.length : 0;
+    count += editedQuery.time_range !== 3 ? 1 : 0;
+
+    return count;
+  }
+
+}
