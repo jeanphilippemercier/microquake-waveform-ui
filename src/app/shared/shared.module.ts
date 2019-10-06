2504d22dfdb12a220dc2ce55313ecd3ced509d90
Tomas Skoda <tomas.skoda@toptal.com>
2019-10-06 16:20:22 +0200

    feat: add ngx-clipboard package

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -21,6 +21,7 @@ import { MomentModule } from 'ngx-moment';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { ToastrModule } from 'ngx-toastr';
 import { NgxFileDropModule } from 'ngx-file-drop';
+import { ClipboardModule } from 'ngx-clipboard';
 
 import { HeaderLayoutComponent } from './layouts/header-layout/header-layout.component';
 import { FooterLayoutComponent } from './layouts/footer-layout/footer-layout.component';
@@ -141,6 +142,7 @@ const matFormFieldDefaultOptions: MatFormFieldDefaultOptions = {
     MomentModule,
     NgxSpinnerModule,
     NgxFileDropModule,
+    ClipboardModule,
     ToastrModule.forRoot(),
     ...MATERIAL_MODULES,
     ...CDK_MODULES,
@@ -157,6 +159,7 @@ const matFormFieldDefaultOptions: MatFormFieldDefaultOptions = {
     MomentModule,
     NgxSpinnerModule,
     NgxFileDropModule,
+    ClipboardModule,
     ToastrModule,
     ...MATERIAL_MODULES,
     ...CDK_MODULES
