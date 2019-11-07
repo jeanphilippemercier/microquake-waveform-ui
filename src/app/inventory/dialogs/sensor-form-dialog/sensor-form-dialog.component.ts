bc8d97f05c16516051fb46cffc555f32f86c5f82
Tomas Skoda <tomas.skoda@toptal.com>
2019-11-07 13:33:52 +0100

    refactor: use form dialog helper class

--- src/app/inventory/dialogs/sensor-form-dialog/sensor-form-dialog.component.ts
+++ src/app/inventory/dialogs/sensor-form-dialog/sensor-form-dialog.component.ts
@@ -1,31 +1,20 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
-import { PageMode } from '@interfaces/core.interface';
 import { SensorFormDialogData } from '@interfaces/dialogs.interface';
+import { FormDialog } from '@core/classes/form-dialog.class';
 
 @Component({
   selector: 'app-sensor-form-dialog',
   templateUrl: './sensor-form-dialog.component.html',
   styleUrls: ['./sensor-form-dialog.component.scss']
 })
-export class SensorFormDialogComponent {
-  PageMode = PageMode;
+export class SensorFormDialogComponent extends FormDialog<SensorFormDialogComponent, SensorFormDialogData> {
 
   constructor(
-    @Inject(MAT_DIALOG_DATA) public dialogData: SensorFormDialogData,
-    private _matDialogRef: MatDialogRef<SensorFormDialogComponent, boolean>,
-  ) { }
-
-  close() {
-    this._matDialogRef.close();
-  }
-
-  onCancelClicked() {
-    this._matDialogRef.close();
-  }
-
-  onSaveClicked() {
-    this._matDialogRef.close(true);
+    protected _matDialogRef: MatDialogRef<SensorFormDialogComponent, boolean>,
+    @Inject(MAT_DIALOG_DATA) public dialogData: SensorFormDialogData
+  ) {
+    super(_matDialogRef, dialogData);
   }
 }
