03947d81e21f4a3612745a697b755b1270304344
Tomas Skoda <tomas.skoda@toptal.com>
2019-11-07 13:33:15 +0100

    refactor: add form-dialog helper class

---
+++ src/app/core/classes/form-dialog.class.ts
@@ -0,0 +1,24 @@
+import { MatDialogRef } from '@angular/material';
+import { PageMode } from '@interfaces/core.interface';
+
+
+export class FormDialog<T, R> {
+  PageMode = PageMode;
+
+  constructor(
+    protected _matDialogRef: MatDialogRef<T, boolean>,
+    public dialogData: R,
+  ) { }
+
+  close() {
+    this._matDialogRef.close();
+  }
+
+  onCancelClicked() {
+    this._matDialogRef.close();
+  }
+
+  onSaveClicked() {
+    this._matDialogRef.close(true);
+  }
+}
