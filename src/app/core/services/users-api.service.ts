f19d3a60db1faa5f667ed9b84d397c0a6fc946ff
Tomas Skoda <tomas.skoda@toptal.com>
2019-11-24 00:14:39 +0100

    refactor(core): clean users-api service
    - remove global api paths

--- src/app/core/services/users-api.service.ts
+++ src/app/core/services/users-api.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import ApiUtil from '@core/utils/api-util';
-import { globals } from '@src/globals';
 import { environment } from '@env/environment';
 import { User } from '@interfaces/user.interface';
 import { UserCreateInput, UserUpdateInput } from '@interfaces/user-dto.interface';
 
+const apiPath = {
+  users: `users`
+};
 
 @Injectable({
   providedIn: 'root'
@@ -19,33 +21,34 @@ export class UsersApiService {
   ) { }
 
 
+
   /**
    * USERS
   */
-  getUsers(query: any = {}): Observable<User[]> {
-    const url = `${environment.apiUrl}users`;
-    const params = ApiUtil.getHttpParams(query);
 
+  getUsers(query: any = {}): Observable<User[]> {
+    const url = `${environment.apiUrl}${apiPath.users}`;
+    const params = ApiUtil.getHttpParams(query);
     return this._http.get<User[]>(url, { params });
   }
 
   getUser(id: number): Observable<User> {
-    const url = `${environment.apiUrl}users/${id}`;
+    const url = `${environment.apiUrl}${apiPath.users}/${id}`;
     return this._http.get<User>(url);
   }
 
   createUser(body: UserCreateInput): Observable<User> {
-    const url = `${environment.apiUrl}users`;
+    const url = `${environment.apiUrl}${apiPath.users}`;
     return this._http.post<User>(url, body);
   }
 
   updateUser(id: number, body: UserUpdateInput): Observable<User> {
-    const url = `${environment.apiUrl}users/${id}`;
+    const url = `${environment.apiUrl}${apiPath.users}/${id}`;
     return this._http.patch<User>(url, body);
   }
 
   deleteUser(id: number): Observable<any> {
-    const url = `${environment.apiUrl}users/${id}`;
+    const url = `${environment.apiUrl}${apiPath.users}/${id}`;
     return this._http.delete(url);
   }
 }
